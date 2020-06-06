b56c186b80be2562c39f329f298139932105adcc
developer1980 <farhad_usa2006@yahoo.co.uk>
2020-06-06 12:42:09 +0430

    first commit

---
+++ bestoon/web/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+from django.conf import settings
+
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Expense(models.Model):
+    text= models.CharField(max_length=255)
+    date=models.DateTimeField()
+    amount=models.BigIntegerField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,)
+    def __str__(self):
+        return '{}-{}'.format(self.date,self.amount)
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date=models.DateTimeField()
+    amount=models.BigIntegerField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,)
+    def __str__(self):
+        return '{}-{}'.format(self.date,self.amount)
